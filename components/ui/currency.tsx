450a45229692534e82214a75848483bfee26c1e1
Mika Sidwell <Mika@invantage.co.za>
2024-09-10 19:49:44 +0200

    Store basics are done. Orders to be finished. Need bussines stuff (i.e. T&C, Shipping, contact-us, Socials, returns...)

---
+++ components/ui/currency.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+const formatter = new Intl.NumberFormat("en-ZA", {
+    style: 'currency',
+    currency: 'ZAR'
+  //   minimumFractionDigits: 2
+  // }).format(this.displaySubTotal)
+});
+
+interface CurrencyProps {
+    value?: string | number;
+}
+
+const Currency: React.FC<CurrencyProps> = ({
+    value
+}) => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if(!isMounted) {
+        return null;
+    }
+
+    return (
+        <div className="font-semibold">
+            {formatter.format(Number(value))}
+        </div>
+    )
+}
+
+export default Currency;
